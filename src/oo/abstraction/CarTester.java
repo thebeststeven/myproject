ef17ac5d6a650bf44e310d082d0c4c98e1f6ea21
user <user@es302n28>
2017-03-14 15:56:07 +0800

    a new jiangouzi

--- src/oo/abstraction/CarTester.java
+++ src/oo/abstraction/CarTester.java
@@ -5,6 +5,8 @@ public class CarTester {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
     Car c1 = new Car("toyota",1800,"altis","sedan");
+    Car c2 = new Car("honda",2000,"Accord","sedan");
+    Car c3 = new Car("audi",1800,"A6","sedan");
 	}
 
 }

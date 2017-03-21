36964532e8055818ef2e278280cd5d3a1dfd5575
user <user@es302n28>
2017-03-21 14:08:29 +0800

    updated

--- src/oo/abstraction/Building.java
+++ src/oo/abstraction/Building.java
@@ -2,13 +2,11 @@ package oo.abstraction;
 
 public class Building {
         String name;
-        String function;
         int floor;
         int person;
         int height;
-        public Building(String name, int height, int floor, int person, String function){
+        public Building(String name, int height, int floor, int person){
         	this.name=name;
-        	this.function=function;
         	this.floor=floor;
         	this.person=person;
         	this.height=height;
---
+++ src/oo/abstraction/BuildingTester.java
@@ -0,0 +1,11 @@
+package oo.abstraction;
+
+public class BuildingTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Building a = new Building("Steven's", 300, 88, 50000);
+		Building b = new Building("Caroline's", 450, 89, 60000);
+	}
+
+}

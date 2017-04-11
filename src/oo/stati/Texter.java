41b795b4ab38d58e86603576871c9dd7298ab87e
user <user@es302n28>
2017-04-11 16:30:31 +0800

    4/11

---
+++ src/oo/stati/Texter.java
@@ -0,0 +1,14 @@
+package oo.stati;
+
+public class Texter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student stu1 = new Student(86, 78, 67);
+		Student stu2 = new Student(65, 87, 34);
+		stu1.pass=70;
+		stu1.print();
+		stu2.print();
+	}
+
+}
